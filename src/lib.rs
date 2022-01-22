12248c71cdae33615214b4c4c0577f269d696f84
Michal Pokrywka <wolfmoon@o2.pl>
2022-01-22 15:34:56 +0100

    Updated to vertigo-0.1.0-beta.3

--- src/lib.rs
+++ src/lib.rs
@@ -1,32 +1,10 @@
-use wasm_bindgen::prelude::wasm_bindgen;
-
-use vertigo::{start_app, VDomComponent};
-
-use vertigo_browserdriver::DriverBrowser;
+use vertigo_browserdriver::start_browser_app;
 
 mod app;
 mod state;
 
-#[global_allocator]
-static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-
-#[wasm_bindgen(start)]
-pub async fn start_application() {
-    // Throw panics in using console.error
-    console_error_panic_hook::set_once();
-
-    // Redirect logging into console
-    wasm_logger::init(wasm_logger::Config::default());
-
-    // Create rendering driver with dependency graph inside
-    let driver = DriverBrowser::new();
-
-    // Create application state lain on this graph
-    let app_state = state::State::new(&driver);
-
-    // Main component
-    let main_component = VDomComponent::new(app_state, app::render);
-
+#[no_mangle]
+pub fn start_application() {
     // Run component in rendering driver
-    start_app(driver, main_component).await;
+    start_browser_app(state::State::new, app::render);
 }
