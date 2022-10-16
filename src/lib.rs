5e99b57e040f13ddb893bbd4ebc360bac19c084f
Michal Pokrywka <wolfmoon@o2.pl>
2022-10-16 09:24:24 +0200

    Updated to vertigo-0.1.0-beta.5

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,21 @@
-mod render;
-mod state;
+use vertigo::{start_app, DomElement, Value, dom, css_fn};
 
-use vertigo::start_app;
+css_fn! { main_div, "
+    color: darkblue;
+" }
+
+fn app() -> DomElement {
+    let message = Value::new("Hello world!".to_string());
+
+    dom! {
+        <div css={main_div()}>
+            "Message to the world: "
+            { message }
+        </div>
+    }
+}
 
 #[no_mangle]
 pub fn start_application() {
-    start_app(state::State::component);
+    start_app(app);
 }
