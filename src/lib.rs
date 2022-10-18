ccc09f6134ae93662231e939399dcd9950bfee6f
Michal Pokrywka <wolfmoon@o2.pl>
2022-10-18 16:43:17 +0200

    Updated tutorial to vertigo-0.1.0-beta.5

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,48 @@
-mod render;
-mod state;
-mod list;
+use vertigo::{start_app, bind, DomElement, Value, dom, css_fn};
 
-use vertigo::start_app;
+mod list;
+use list::List;
+
+css_fn! { main_div, "
+    color: darkblue;
+" }
+
+fn app() -> DomElement {
+    let message = Value::new("Hello world!".to_string());
+    let strong = Value::new(true);
+
+    let my_items = Value::new(
+        vec![
+            "Item1".to_string(),
+            "Item2".to_string(),
+        ]
+    );
+
+    let message_element = strong.render_value(move |strong|
+        if strong {
+            dom! { <strong>{&message}</strong> }
+        } else {
+            dom! { <span>{&message}</span> }
+        }
+    );
+
+    let switch = bind(&strong).call(|ctx, strong|
+        strong.set(
+            !strong.get(ctx)
+        )
+    );
+
+    dom! {
+        <div css={main_div()}>
+            "Message to the world: "
+            {message_element}
+            <button on_click={switch}>"Switch"</button>
+            <List items={my_items}/>
+        </div>
+    }
+}
 
 #[no_mangle]
 pub fn start_application() {
-    start_app(state::State::component);
+    start_app(app);
 }
--- src/list.rs
+++ src/list.rs
@@ -1,4 +1,45 @@
-use vertigo::{Computed, Value, DomElement, dom, bind, bind2, css, Css};
+use vertigo::{DomElement, css, Css, dom, Value, bind, bind2};
+
+pub struct List {
+    pub items: Value<Vec<String>>,
+}
+
+impl List {
+    pub fn mount(self) -> DomElement {
+        let new_item = Value::<String>::default();
+        let count = self.items.map(|items| items.len());
+
+        let add = bind2(&self.items, &new_item).call(|ctx, items, new_item| {
+            let mut items_vec = items.get(ctx).to_vec();
+            items_vec.push(new_item.get(ctx));
+            items.set(items_vec);
+            new_item.set("".to_string());
+        });
+
+        let change = bind(&new_item).call_param(|_ctx, new_item, new_value| {
+            new_item.set(new_value);
+        });
+
+        let elements = self.items.render_list(
+            |item| item.clone(),
+            |item| {
+                let excl = item.ends_with('!');
+                dom! { <li css={alternate_rows(excl)}>{item}</li> }
+            },
+        );
+
+        dom! {
+            <div>
+                <p>"My list (" {count} ")"</p>
+                <ol>
+                    { elements }
+                </ol>
+                <input value={new_item.to_computed()} on_input={change} />
+                <button on_click={add}>"Add"</button>
+            </div>
+        }
+    }
+}
 
 fn alternate_rows(excl: bool) -> Css {
     let bg_color = if excl { "yellow" } else { "inherit" };
@@ -11,65 +52,4 @@ fn alternate_rows(excl: bool) -> Css {
             color: blue;
         };
     ")
-}
-
-#[derive(Clone)]
-pub struct State {
-    items: Value<Vec<String>>,
-    new_item: Value<String>,
-    count: Computed<usize>,
-}
-
-impl State {
-    pub fn new() -> Self {
-        let items = Value::new(vec![
-            "Item 1".to_string(),
-            "Item 2".to_string(),
-        ]);
-
-        let count = {
-            let items = items.clone();
-            Computed::from(move |ctx| items.get(ctx).len())
-        };
-
-        State {
-            items,
-            new_item: Value::new(String::default()),
-            count,
-        }
-    }
-
-    pub fn render(&self) -> DomElement {
-        let add = bind2(&self.items, &self.new_item).call(|ctx, items, new_item| {
-            let mut items_vec = items.get(ctx).to_vec();
-            items_vec.push(new_item.get(ctx));
-            items.set(items_vec);
-            new_item.set("".to_string());
-        });
-
-        let change = bind(&self.new_item).call_param(|_ctx, new_item, new_value| {
-            new_item.set(new_value);
-        });
-
-        let elements = self.items.render_list(
-            |item| item.clone(),
-            |item| {
-                let excl = item.ends_with('!');
-                dom! {
-                    <li css={alternate_rows(excl)}>{item}</li>
-                }
-            },
-        );
-
-        dom! {
-            <div>
-                <p>"My list (" { &self.count } ")"</p>
-                <ol>
-                    { elements }
-                </ol>
-                <input value={self.new_item.to_computed()} on_input={change} />
-                <button on_click={add}>"Add"</button>
-            </div>
-        }
-    }
 }
